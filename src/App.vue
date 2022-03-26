eb286cb1172677c7e9c9a9c005ca2acb3a5dd8e0
Dennis Wenrich <dennisfinley@gmail.com>
2022-03-26 13:59:32 -0400

    removed exisiting components / created own

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <newGreeting/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import newGreeting from './components/greeting.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    newGreeting
   }
-}
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/greeting.vue
@@ -0,0 +1,14 @@
+<template>
+    <p>{{ msg }}</p>
+</template>
+
+<script>
+export default {
+    name: 'newGreeting',
+    data(){
+        return {
+            msg: 'Hello World!'
+        }
+    }
+}
+</script>
